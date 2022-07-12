c656807b93d4ba1fd60a590a316c72c02ac49300
BrunoAlz <bruno6821@gmail.com>
2022-07-12 01:09:17 -0300

    Enviando Arquivos com Ajax e Promise.All()
    Criando o método para o Upload dos arquivos com ajax e promises,
    Pequena alteração no input #files com a propriedade multiple

--- app/public/src/controller/DopBoxController.js
+++ app/public/src/controller/DopBoxController.js
@@ -14,7 +14,7 @@ class DropBoxController{
 
     }
 
-    // Função Inicializadora dos Eventos
+    // Método Inicializador dos Eventos
     initEvents(){
         
         // Adiciona a Escuta de Eventos no botão send files
@@ -28,9 +28,65 @@ class DropBoxController{
         // Adiciona a Escuta de Eventos no Input, e no change "Mudança", ou seja, quando forem selecionados arquivos, pega o evento
         this.inputFilesEl.addEventListener('change', event => {
 
+            this.uploadTask(event.target.files);
+
             // Quando ocorrer o evento, a mudança, adiciona o CSS, para exibir o modal via CSS
             this.snackModalEl.style.display = 'block'
 
         });
+    }
+
+    // Método para Upload de Arquivos
+    // Cria um array vazio para guardar os Files, e cada arquivo terá uma promessa com o promises
+    uploadTask(files){
+        
+        // Array vazio para receber os arquivos
+        let promises = [];
+
+       /*  Como não sabemos a quantidade, faremos um loop por todos os arquiovos, que é uma Coleção,
+        E depois fazemos o unpack dos arquivos no array com o Operado spread ... */
+        [...files].forEach(file => {
+
+            // Para cada arquivo adicionado no array, faremos uma promise
+            promises.push(new Promise((resolve, reject) => {
+
+                /* Realizando a requisição AJAX para cada Promessa
+                Criando a Variavel ajax */
+                let ajax = new XMLHttpRequest();
+
+                // Abrindo a requisição via POST, e mandando para a pasta /upload
+                ajax.open('POST', '/upload');
+
+                // Verificaremos se a Requisição ajax foi feita com onload
+                ajax.onload = event => {                    
+                    // Após pegar o evento onload
+                    // Testamos, para ver se a resposta do ajax é aceita
+                    try {                        
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) {
+                        reject(e);                        
+                    }
+                };
+
+                // Se ocorrer erro na requisição ajax, ja pegamos esse evento e passamos para o reject da promise
+                ajax.onerror = event => {
+                    reject(event);
+                };
+
+                // Usaremos o FormData para passar os arquivos para o Ajax
+                let formData = new FormData();
+
+                // pegamos os arquivos que estão passando pelo forEach com a variavel file
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+
+            }));
+
+        });        
+
+        // Adicionando o array ao promise all, caso todas promessas passarem retorna, se falhar rejeita
+        return Promise.all(promises);
+
     }
 }
