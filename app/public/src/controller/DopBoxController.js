691507776e26f8b39806c3353602aa0afd07e2ef
BrunoAlz <bruno6821@gmail.com>
2022-07-12 01:41:18 -0300

    Atualizando a view com o Upload dos arquivos
    Foram adicionados o métodos para mostrar o modal de upload, bem como o progresso da barra de progresso.
    
    Foram adicionados os calculos para medir o tempo restante para o Upload, bem como atualização dinâmica do nome do arquivo que está subindo.

--- app/public/src/controller/DopBoxController.js
+++ app/public/src/controller/DopBoxController.js
@@ -8,6 +8,12 @@ class DropBoxController{
         this.inputFilesEl = document.querySelector('#files');
         // Recupera o Elemento Modal da Barra de Upload
         this.snackModalEl = document.querySelector('#react-snackbar-root');
+        // Recupera o Elemento Barra de Upload para tornar o carregamento da barra dinamico
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+        // Recupera o Elemento label que exibe o nome do documento que está sendo feito upload
+        this.nameFileEl = this.snackModalEl.querySelector('.filename');
+        // Recupera o Elemento labe que exibe o tempo restante para o Upload do arquivo
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft');
 
         // Chama a função de Inicialização dos Eventos
         this.initEvents();        
@@ -30,10 +36,19 @@ class DropBoxController{
 
             this.uploadTask(event.target.files);
 
-            // Quando ocorrer o evento, a mudança, adiciona o CSS, para exibir o modal via CSS
-            this.snackModalEl.style.display = 'block'
+            // Quando ocorrer o evento, "a mudança" chama o método modalSHow
+            this.modalShow();
+
+            // Limpando o Elemento Input, após o evento change
+            this.inputFilesEl.value = '';
 
         });
+    }
+
+    // Método para mostrar e esconder o modal durante os Uploads
+    modalShow(show = true){
+        // se a váriavel show for true, adiciona a classe block, se for false, adiciona a classe none.
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
     }
 
     // Método para Upload de Arquivos
@@ -58,7 +73,11 @@ class DropBoxController{
                 ajax.open('POST', '/upload');
 
                 // Verificaremos se a Requisição ajax foi feita com onload
-                ajax.onload = event => {                    
+                ajax.onload = event => {   
+                    
+                    // Fechando a modal de upload, com o método modalShow
+                    this.modalShow(false);
+                    
                     // Após pegar o evento onload
                     // Testamos, para ver se a resposta do ajax é aceita
                     try {                        
@@ -70,7 +89,19 @@ class DropBoxController{
 
                 // Se ocorrer erro na requisição ajax, ja pegamos esse evento e passamos para o reject da promise
                 ajax.onerror = event => {
+
+                    // Fechando a modal de upload, com o método modalShow
+                    this.modalShow(false);
+
                     reject(event);
+                };
+
+                //    
+                ajax.upload.onprogress = event => {
+
+                    // Chamando o método UploadProgress que recebe o evento de Upload e o arquivo
+                    this.uploadProgress(event, file);
+
                 };
 
                 // Usaremos o FormData para passar os arquivos para o Ajax
@@ -78,6 +109,9 @@ class DropBoxController{
 
                 // pegamos os arquivos que estão passando pelo forEach com a variavel file
                 formData.append('input-file', file);
+
+                // recuperando a hora no momento que o upload irá começar
+                this.startUploadTime = Date.now();
 
                 // Enviando a requisição
                 ajax.send(formData);
@@ -90,4 +124,56 @@ class DropBoxController{
         return Promise.all(promises);
 
     }
+
+    // Método para atualizar a barra de progresso
+    uploadProgress(event, file){
+
+        // Calculando o tempo gasto no upload do arquivo
+        let timespent = Date.now() - this.startUploadTime
+
+        // pega o atributo loaded, dentro do event de upload
+        let loaded = event.loaded;
+
+        // pega o atributo total, dentro do event de upload
+        let total = event.total;
+
+        // faz a regra de 3 para saber a porcentagem carregada
+        let porcent = parseInt((loaded / total) * 100);
+
+        // Calculando o tempo restante para o upload
+        let timeleft = ((100 - porcent) * timespent) / porcent
+        
+        // atualiza a propriedade css width da progress bar com a porcentagem atual do upload
+        this.progressBarEl.style.width = `${porcent}%`
+
+        // atualizando o label como o arquivo que esta subindo
+        this.nameFileEl.innerHTML = file.name;
+
+        // atualizando o tempo que falta para terminar o Upload
+        this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeleft);
+
+        console.log(timespent, timeleft, porcent);
+    };
+
+    // Método para formatar o tempo de upload de milisegundos para um formato amigável
+    formatTimeToHuman(duration){
+        
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) {
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+        }
+
+        if (minutes > 0) {
+            return `${minutes} minutos e ${seconds} segundos`;
+        }
+
+        if (seconds > 0) {
+            return `${seconds} segundos`;
+        }
+        return '';
+
+    };
 }
